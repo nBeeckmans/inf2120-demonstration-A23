ba8543160fb37a46b188da5414f4982908d1c688
nBeeckmans <nicolas.beeckmans@hotmail.com>
2023-09-07 16:05:42 -0400

    pre-finalisation

--- ReferenceRevision/Taxe/Facture.java
+++ ReferenceRevision/Taxe/Facture.java
@@ -1,33 +1,68 @@
 package ReferenceRevision.Taxe;
 
 public class Facture {
+
+    static final String MESSAGE_ERREUR =
+            "impossible d'ajouter un element supplementaire a ";
+    static final int NOMBRE_MAXIMUM_ELEMENTS = 10;
     NonTaxable[] produitsNonTaxables = new NonTaxable[10];
     TaxeSimple[] produitsTaxablesSimple = new TaxeSimple[10];
+    int compteurElementTaxablesSimple = 0;
     TaxeDouble[] produitsTaxablesDouble = new TaxeDouble[10];
+    int compteurElementTaxablesDouble = 0;
 
     //ajouterProduit(NonTaxable produit);
     //ajouterProduit(TaxeSimple produit);
     //ajouterProduit(TaxeDouble produit);
-    public void ajouterProduitNonTaxable(NonTaxable produit) {
 
+    //utilisation de null pour montrer que la case est vide !
+    public void ajouterProduitNonTaxable(NonTaxable produit) {
+        int i = 0;
+        NonTaxable element= produitsNonTaxables[i];
+        while(element != null && i < produitsNonTaxables.length) {
+            i++;
+            element = produitsNonTaxables[i];
+        }
+        if (i < produitsNonTaxables.length) {
+            produitsNonTaxables[i] = produit;
+        } else {
+            System.err.println(MESSAGE_ERREUR + NonTaxable.class.getName());
+        }
     }
     public void ajouterProduitTaxeSimple(TaxeSimple produit) {
+        if ( compteurElementTaxablesSimple < NOMBRE_MAXIMUM_ELEMENTS ) {
+            produitsTaxablesSimple[compteurElementTaxablesSimple] = produit;
+            compteurElementTaxablesSimple ++;
+        } else {
+            System.err.println(MESSAGE_ERREUR + TaxeSimple.class.getName());
+        }
 
     }
     public void ajouterProduitTaxeDouble(TaxeDouble produit) {
-
+        if ( compteurElementTaxablesDouble < NOMBRE_MAXIMUM_ELEMENTS ) {
+            produitsTaxablesDouble[compteurElementTaxablesDouble] = produit;
+            compteurElementTaxablesDouble ++;
+        } else {
+            System.err.println(MESSAGE_ERREUR + TaxeSimple.class.getName());
+        }
     }
 
     public double prixTotal() {
         double prixTotal = 0.0;
-        for (NonTaxable produits: produitsNonTaxables) {
-            prixTotal += produits.prix();
+        //for normal
+        for (int i = 0; i < NOMBRE_MAXIMUM_ELEMENTS; i++) {
+            if (produitsNonTaxables[i] != null) {
+                prixTotal += produitsNonTaxables[i].prix();
+            }
         }
+        //for ameliore
         for (TaxeSimple produits: produitsTaxablesSimple) {
-            prixTotal += produits.prix();
+            if (produits != null)
+                prixTotal += produits.prix();
         }
-        for (TaxeDouble produits: produitsTaxablesDouble) {
-            prixTotal += produits.prix();
+        //for "optimise"
+        for (int i = 0; i < compteurElementTaxablesDouble; i ++){
+                prixTotal += produitsTaxablesDouble[i].prix();
         }
         return prixTotal;
     }
